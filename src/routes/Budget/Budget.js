2040bbbd6d69e61eeabe158e83f9430b8c3facd9
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-06-22 15:43:47 -0600

    Basic budget page

---
+++ src/routes/Budget/Budget.js
@@ -0,0 +1,46 @@
+import "./styles/budget.css";
+import Item from "./Item/Item";
+import { Fab, Typography } from "@material-ui/core";
+import Adder from "./Adder/Adder";
+import AddIcon from "@material-ui/icons/Add";
+
+import useBudget from "./useBudget";
+import { green } from "@material-ui/core/colors";
+import { Dialog } from "@material-ui/core";
+
+const Budget = (props) => {
+  const [items, addItem, deleteItem, updateItem] = useBudget(props);
+
+  return (
+    <div className="wrapper">
+      <div>
+        <div className="title">
+          <Typography variant="h4">Build Team Purchase List</Typography>
+        </div>
+
+        {items.map((item, index) => {
+          return (
+            <Item
+              key={index}
+              {...item}
+              itemIndex={index}
+              deleteItem={deleteItem}
+              updateItem={updateItem}
+            />
+          );
+        })}
+      </div>
+
+      <div className="button-section">
+        <Fab color="secondary" onClick={() => addItem(items[0])}>
+          <AddIcon />
+        </Fab>
+      </div>
+
+      <Dialog open={false}></Dialog>
+      {/* <Adder addItem={addItem} /> */}
+    </div>
+  );
+};
+
+export default Budget;
---
+++ src/routes/Budget/useBudget.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import defaultItems from "./defaultItems";
+
+const useBudget = (props) => {
+  const [items, setItems] = useState(defaultItems);
+  const [open, setOpen] = useState(false);
+
+  const addItem = (item) => {
+    setItems([...items, item]);
+  };
+
+  const deleteItem = (itemIndex) => {
+    let newItems = [...items];
+    newItems.splice(itemIndex, 1);
+
+    setItems(newItems);
+  };
+
+  const updateItem = (item, itemIndex) => {
+    let newItems = [...items];
+    newItems[itemIndex] = item;
+
+    setItems(newItems);
+  };
+
+  return [items, addItem, deleteItem, updateItem];
+};
+
+export default useBudget;
