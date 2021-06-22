c862258133d82ca62b958b89a923120f3b07de7b
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-06-22 15:47:04 -0600

    Created Vendor component

---
+++ src/routes/Budget/Item/Vendor/Vendor.js
@@ -0,0 +1,18 @@
+import { Tooltip, Button } from "@material-ui/core";
+import "./vendor.css";
+
+const Vendor = ({ vendorName, vendorLogo, url }) => {
+  return (
+    <Tooltip title={vendorName} placement="bottom">
+      <Button
+        href={url}
+        target="blank"
+        style={{ padding: 0, minWidth: 0, backgroundColor: "transparent" }}
+      >
+        <img src={vendorLogo} alt={vendorName} className="vendor-logo"></img>
+      </Button>
+    </Tooltip>
+  );
+};
+
+export default Vendor;
