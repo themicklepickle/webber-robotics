fe127dce44687784c7da75c6420bd7c63148da57
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-06-22 15:40:39 -0600

    Added theming

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@material-ui/core/styles";
+import "@fontsource/roboto";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto",
+  },
+});
+
+export default theme;
