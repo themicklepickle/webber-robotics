39546112744c91ac710d41a1ca6aff5d51853136
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-06-30 18:40:56 -0600

    Made cyan and deep orange theme colors

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,15 @@
-import { createTheme } from "@material-ui/core/styles";
 import "@fontsource/roboto";
+
+import { createTheme } from "@material-ui/core/styles";
+import { cyan, deepOrange } from "@material-ui/core/colors";
 
 const theme = createTheme({
   typography: {
     fontFamily: "Roboto",
+  },
+  palette: {
+    primary: cyan,
+    secondary: deepOrange,
   },
 });
 
