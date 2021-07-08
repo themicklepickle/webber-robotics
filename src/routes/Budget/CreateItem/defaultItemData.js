78826288db139ebd59e71378cbbd4f0c270f2e71
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-07-08 16:19:12 -0600

    TEMP: default item data (to be fetched from API)

---
+++ src/routes/Budget/CreateItem/defaultItemData.js
@@ -0,0 +1,43 @@
+const item1 = {
+  name: "45MM U CHANNEL - 440MM",
+  description: "Need these for securing things together",
+  vendor: {
+    name: "Home Depot",
+    logo: "../images/vendors/home-depot-ca.jpeg",
+  },
+  unitPrice: 57.9,
+  unitPriceCurrency: "USD",
+  url: "https://www.homedepot.ca/product/micropro-sienna-2-x-4-x-8-pressure-treated-wood-above-ground-use-only-/1000789777",
+  image:
+    "https://cdn11.bigcommerce.com/s-t3eo8vwp22/images/stencil/1280x1280/products/597/1943/HDHex-No_Gearbox-New_Sticker__13371.1618523233.png?c=2",
+};
+
+const item2 = {
+  name: "Tape",
+  description: "Need these for securing things together",
+  vendor: {
+    name: "Canadian Tire",
+    logo: "../images/vendors/canadian-tire.webp",
+  },
+  unitPrice: 5,
+  unitPriceCurrency: "EUR",
+  url: "https://www.canadiantire.ca/en/pdp/mastercraft-high-visibility-pliers-set-6-pc-0584789p.html#srp",
+  image:
+    "https://cdn11.bigcommerce.com/s-t3eo8vwp22/images/stencil/1280x1280/products/597/1943/HDHex-No_Gearbox-New_Sticker__13371.1618523233.png?c=2",
+};
+
+const item3 = {
+  name: "7mm x 7mm Black Plastic Flexible Nested Semi Closed Drag Chain Cable Wire Carrier 1M 3606 Series Mecanum Wheel Set (Bearing Supported Rollers, 100mm Diameter) - 4 Pack",
+  description: "For the robot to move around",
+  vendor: {
+    name: "Rev Robotics",
+    logo: "https://cdn11.bigcommerce.com/s-t3eo8vwp22/images/stencil/original/rev_logo-250x100px_1597946647__83857.original.png",
+  },
+  unitPrice: 5,
+  unitPriceCurrency: "CAD",
+  url: "https://www.revrobotics.com/rev-45-1896/",
+  image:
+    "https://cdn11.bigcommerce.com/s-t3eo8vwp22/images/stencil/1280x1280/products/597/1943/HDHex-No_Gearbox-New_Sticker__13371.1618523233.png?c=2",
+};
+
+export { item1, item2, item3 };
