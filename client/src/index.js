1566480145f2d42cbe1adb1d05f5c743b70f41e1
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-07-31 15:38:19 -0600

    Setup ApolloProvider for development (local uri)

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,22 @@ import ReactDOM from "react-dom";
 
 import { ThemeProvider } from "@material-ui/core/styles";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import App from "./App";
 import theme from "./theme";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
