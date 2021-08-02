f4f3f39a785ec570739eb2604132760daa833ca3
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-08-01 23:15:51 -0600

    Updated IsPurchasedCheckBox - call mutation directly

--- client/src/routes/Budget/Item/IsPurchasedCheckBox/IsPurchasedCheckBox.js
+++ client/src/routes/Budget/Item/IsPurchasedCheckBox/IsPurchasedCheckBox.js
@@ -1,16 +1,43 @@
-import { IconButton } from "@material-ui/core";
+import { IconButton, Tooltip } from "@material-ui/core";
+import { green } from "@material-ui/core/colors";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 
-import useIsPurchasedCheckBox from "./useIsPurchasedCheckBox";
+import { useState } from "react";
 
-const IsPurchasedCheckBox = ({ isChecked, datePurchased, check, uncheck }) => {
-  const { icon, toggleCheck } = useIsPurchasedCheckBox(
-    isChecked,
-    datePurchased,
-    check,
-    uncheck
+const IsPurchasedCheckBox = ({
+  itemId,
+  initialIsChecked,
+  initialDatePurchased,
+}) => {
+  const [isChecked, setIsChecked] = useState(initialIsChecked);
+  const [datePurchased, setDatePurchased] = useState(initialDatePurchased);
+
+  const check = () => {
+    setIsChecked(true);
+    setDatePurchased(new Date());
+  };
+
+  const uncheck = () => {
+    setIsChecked(false);
+    setDatePurchased(null);
+  };
+
+  if (isChecked)
+    <IconButton onClick={uncheck}>
+      <Tooltip
+        title={`Purchased on ${datePurchased.toDateString()}`}
+        placement="bottom"
+      >
+        <CheckBoxIcon style={{ color: green[500] }}></CheckBoxIcon>
+      </Tooltip>
+    </IconButton>;
+
+  return (
+    <IconButton onClick={check}>
+      <CheckBoxOutlineBlankIcon></CheckBoxOutlineBlankIcon>
+    </IconButton>
   );
-
-  return <IconButton onClick={toggleCheck}>{icon}</IconButton>;
 };
 
 export default IsPurchasedCheckBox;
