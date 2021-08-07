be6f6f6502f951d8d6872d62d9737f7523416059
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-08-07 00:01:11 -0600

    Created ITEM_DETAILS query

--- client/src/graphql/queries/index.js
+++ client/src/graphql/queries/index.js
@@ -1,1 +1,2 @@
 export { default as ITEMS } from "./items";
+export { default as ITEM_DETAILS } from "./itemDetails";
---
+++ client/src/graphql/queries/itemDetails.js
@@ -0,0 +1,20 @@
+import { gql } from "@apollo/client";
+
+const ITEM_DETAILS = gql`
+  query GetItemDetails($url: String!) {
+    itemDetails(url: $url) {
+      name
+      unitPrice
+      unitPriceCurrency
+      url
+      image
+      vendor {
+        name
+        logo
+        domain
+      }
+    }
+  }
+`;
+
+export default ITEM_DETAILS;
