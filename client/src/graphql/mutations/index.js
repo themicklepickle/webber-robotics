f8afaaeae95727b23b491b1f3044e1beb5ee8ffe
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-08-10 23:51:57 -0600

    Created client-side deleteItem mutation call

--- client/src/graphql/mutations/index.js
+++ client/src/graphql/mutations/index.js
@@ -1,4 +1,5 @@
 export { default as UPDATE_ITEM_PURCHASED } from "./updateItemPurchased";
 export { default as UPDATE_ITEM_QUANTITY } from "./updateItemQuantity";
 export { default as CREATE_ITEM } from "./createItem";
+export { default as DELETE_ITEM } from "./deleteItem";
 export { default as UPDATE_ITEM } from "./updateItem";
