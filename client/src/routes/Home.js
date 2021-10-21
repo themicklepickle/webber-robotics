9c2646bfd057f94bd77bfe8ba210cfb81b27308f
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-10-21 11:45:18 -0600

    Spongebob homepage temp

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,7 +1,13 @@
 import { Header } from "../components";
 
 const Home = () => {
-  return <Header title="Webber Robotics"></Header>;
+  return (
+    <>
+      <Header title="Webber Robotics"></Header>
+      <img src="Mr. Krabs.png" />
+      <img src="Gary.png" />
+    </>
+  );
 };
 
 export default Home;
